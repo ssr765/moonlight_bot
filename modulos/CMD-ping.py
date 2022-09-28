1fd9291c576e9b8ea51f2c564758b7975cbd0d7a
ssr765 <64972770+ssr765@users.noreply.github.com>
2022-09-29 01:51:46 +0200

    Update CMD-ping.py with new configuration.

--- modulos/CMD-ping.py
+++ modulos/CMD-ping.py
@@ -8,17 +8,10 @@ class ping(commands.Cog):
         super().__init__()
         self.client = client
 
-    @app_commands.command(
-        name="ping",
-        description="Muestra la latencia del bot."
-        )
+    @app_commands.command(name="ping", description="Muestra la latencia del bot.")
     async def ping(self, interaction: discord.Interaction):
-        embed = discord.Embed(color=0xdcdcdc)
-        embed.set_author(
-            name=f"Ping: {round(self.client.latency * 1000)} ms",
-            icon_url=self.client.user.display_avatar
-            )
-            
+        embed = discord.Embed(color=self.client.config.embed_color)
+        embed.set_author(name=f"Ping: {round(self.client.latency * 1000)} ms", icon_url=self.client.user.display_avatar)
         await interaction.response.send_message(embed=embed)
 
 async def setup(client: commands.Bot):

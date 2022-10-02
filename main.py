d7bbebddbaa48c1105190393deee480e4b570d7f
ssr765 <64972770+ssr765@users.noreply.github.com>
2022-10-02 21:51:04 +0200

    Fixed an error, database port was a str

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = int(os.getenv("DB_PORT"))
 DB_DATABASE = os.getenv("DB_DATABASE")
 
 class client(commands.Bot):

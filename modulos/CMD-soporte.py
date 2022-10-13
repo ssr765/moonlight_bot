a23c424cf39c863d2445288d4da93854a3fe0e7a
ssr765 <64972770+ssr765@users.noreply.github.com>
2022-10-13 21:12:46 +0200

    Update CMD-soporte.py

--- modulos/CMD-soporte.py
+++ modulos/CMD-soporte.py
@@ -10,7 +10,7 @@ class soporte(commands.Cog):
 
     @app_commands.command(name="soporte", description="Invitación al servidor de soporte de moonlight🌙.")
     async def soporte(self, interaction: discord.Interaction):
-        embed = discord.Embed(description="Si tienes alguna sugerencia, has encontrado un fallo o tienes alguna duda, entra al servidor de soporte de moonlight\\🌙\n\n> https://discord.gg/X569K45f6r", color=self.client.config.embed_color)
+        embed = discord.Embed(description="Si tienes alguna sugerencia, has encontrado un fallo o tienes alguna duda, entra al servidor de soporte de moonlight\\🌙\n\n> [Click aquí para entrar al servidor](https://discord.gg/X569K45f6r)", color=self.client.config.embed_color)
         embed.set_author(name=f"Soporte de moonlight🌙", url="https://discord.gg/X569K45f6r", icon_url=self.client.user.display_avatar)
         await interaction.response.send_message(embed=embed)
 
